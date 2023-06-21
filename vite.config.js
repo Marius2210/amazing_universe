821b5baa2d08d28b83cc6681e6e48612debfd5c7
Mario <marioernestomelendez@gmail.com>
2023-06-20 19:53:42 -0600

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Marius2210.github.io/amazing_universe/',
+  base: 'https://marius2210.github.io/amazing_universe/',
 })
