8dd16b1137c603fb4490250a76947447f5cdccc6
Mario <marioernestomelendez@gmail.com>
2023-06-24 19:30:09 -0600

    commit 8

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import BurguerButton from "./BurguerButton"
 import React, { useState } from "react";
+import Title from './assets/images/title.png';
 
 const Navigation = () => {
 
@@ -15,7 +16,7 @@ const Navigation = () => {
     return ( 
       <nav className="navigation">
         <div className="title">
-        <Link to="/"><img src={`src/assets/images/saturn.png`}/></Link>
+        <Link to="/"><img src={Title}/></Link>
         </div>
         <ul className={`nav-main ${clicked ? "active" : ""}`}>
             <li>
