3b43b72b0d59727cc02e1263f9ad88e396b2dd19
Mario <marioernestomelendez@gmail.com>
2023-06-25 12:58:35 -0600

    commit 12

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
     <Navigation/>
-    <Start/>
     <Routes>
       <Route path='/' element={<Start/>}/>
       <Route path='curiosities' element={<Curiosities/>}/>
